b57b6ec26737c784492615f58a57421600387637
hm90079007 <hm90079007@gmail.com>
2020-10-07 09:26:12 +0900

    add

--- main.go
+++ main.go
@@ -1,24 +1,56 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"math/rand"
-	"time"
+	"image"
+	_ "image/jpeg"
+	"image/png"
+	_ "image/png"
+	"os"
 )
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
-	n := rand.Intn(5)
-
-	switch n {
-	case 1:
-		fmt.Println("大吉")
-	case 2, 3:
-		fmt.Println("中吉")
-	case 4, 5:
-		fmt.Println("小吉")
-	case 6:
-		fmt.Println("凶")
+	// 入力画像パス.
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("Input source image file path  >>")
+	if !scanner.Scan() {
+		fmt.Println("Please input source image file path.")
+		return
 	}
+	srcPath := scanner.Text()
 
+	// ファイルオープン
+	file, err := os.Open(srcPath)
+	assert(err, "Invalid image file path "+srcPath)
+	defer file.Close()
+
+	// ファイルオブジェクトを画像オブジェクトに変換
+	img, _, err := image.Decode(file)
+	assert(err, "Failed to convert file to image.")
+
+	// 出力画像パス.
+	fmt.Print("Input output image file path  >>")
+	if !scanner.Scan() {
+		fmt.Println("Please input output image file path.")
+		return
+	}
+	dstPath := scanner.Text()
+
+	// 出力ファイルを生成
+	out, err := os.Create(dstPath)
+	assert(err, "Failed to create destination path.")
+	defer out.Close()
+
+	// 画像ファイル出力
+	//    jpeg.Encode(out, img, nil)
+	png.Encode(out, img)
+
+}
+
+// errorオブジェクトをチェックし、nilの場合例外を送出
+func assert(err error, msg string) {
+	if err != nil {
+		panic(err.Error() + ":" + msg)
+	}
 }
